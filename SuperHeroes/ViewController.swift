e429c9ef089f240d06ddb85ffd3bd3879b6db9f4
DanyST <lherreralillo@gmail.com>
2023-09-30 18:14:51 -0300

    create project and .gitignore

---
+++ SuperHeroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperHeroes
+//
+//  Created by Luis Herrera Lillo on 30-09-23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
