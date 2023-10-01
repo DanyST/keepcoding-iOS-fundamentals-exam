d05916e06a116ca601501b17f9ed3c7498054e07
DanyST <lherreralillo@gmail.com>
2023-10-01 15:43:15 -0300

    Add TransformationListRequest

---
+++ SuperHeroes/Features/TransformationsList/Models/TransformationListRequest.swift
@@ -0,0 +1,12 @@
+//
+//  TransformationListRequest.swift
+//  SuperHeroes
+//
+//  Created by Luis Herrera Lillo on 01-10-23.
+//
+
+import Foundation
+
+struct TransformationListRequest: Encodable {
+    let id: String
+}
