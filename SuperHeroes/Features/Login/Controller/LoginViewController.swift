b7f4c2356e658cc90d8ab7597f3ec09925d6ce76
DanyST <lherreralillo@gmail.com>
2023-10-01 16:04:08 -0300

    add init with generic NetworkModel to LoginViewController and login with continue button

--- SuperHeroes/Features/Login/Controller/LoginViewController.swift
+++ SuperHeroes/Features/Login/Controller/LoginViewController.swift
@@ -7,9 +7,21 @@
 
 import UIKit
 
-final class LoginViewController: UIViewController {
+final class LoginViewController<NetworkModel: NetworkDataModelProtocol>: UIViewController {
     // MARK: - Subviews
     private lazy var innerView = LoginView()
+    
+    // MARK: - Properties
+    let networkModel: NetworkModel
+    
+    init(networkModel: NetworkModel = NetworkDataModel()) {
+        self.networkModel = networkModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     // MARK: - LifeCycle
     override func loadView() {
@@ -25,8 +37,25 @@ final class LoginViewController: UIViewController {
 // MARK: - LoginViewDelegate
 extension LoginViewController: LoginViewDelegate {
     func continueButtonDidTapped() {
-        print("continueButtonDidTapped")
-        let viewController = HeroesListViewController()
-        navigationController?.setViewControllers([viewController], animated: true)
+        guard
+            let userName = innerView.usernameTextField.text,
+            let password = innerView.passwordTextField.text
+        else {
+            print("No data")
+            return
+        }
+        
+        networkModel.login(
+            user: userName,
+            password: password
+        ) { [weak self] result in
+            switch result {
+            case .success(_):
+                let viewController = HeroesListViewController()
+                self?.navigationController?.setViewControllers([viewController], animated: true)
+            case let .failure(error):
+                print("Error: \(error)")
+            }
+        }
     }
 }
