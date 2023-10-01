0d5aabf490a7869146c38259a82f04c46ec645f7
DanyST <lherreralillo@gmail.com>
2023-10-01 15:13:51 -0300

    add navigation from HeroesListViewController to HeroDetailViewController

--- SuperHeroes/Features/HeroesList/Controller/HeroesListViewController.swift
+++ SuperHeroes/Features/HeroesList/Controller/HeroesListViewController.swift
@@ -32,7 +32,13 @@ extension HeroesListViewController {
 extension HeroesListViewController {
     private func setupGenericTableViewController() -> GenericTableViewController<Hero, CharacterInfoTableViewCell> {
         let genericTableViewController = GenericTableViewController(
-            items: [Hero(id: "", name: "Maestro Roshi", description: "Es un maestro de artes marciales que tiene una escuela, donde entrenará a Goku y Krilin para los Torneos de Artes Marciales. Aún en los primeros episodios había un toque de tradición y disciplina, muy bien representada por el maestro. Pero Muten Roshi es un anciano extremadamente pervertido con las chicas jóvenes, una actitud que se utilizaba en escenas divertidas en los años 80. En su faceta de experto en artes marciales, fue quien le enseñó a Goku técnicas como el Kame Hame Ha", photo: URL(string: "https://cdn.alfabetajuega.com/alfabetajuega/2020/06/Roshi.jpg?width=300")!, favorite: false)],
+            items: [
+                Hero(id: "",
+                     name: "Maestro Roshi",
+                     description: "Es un maestro de artes marciales que tiene una escuela, donde entrenará a Goku y Krilin para los Torneos de Artes Marciales. Aún en los primeros episodios había un toque de tradición y disciplina, muy bien representada por el maestro. Pero Muten Roshi es un anciano extremadamente pervertido con las chicas jóvenes, una actitud que se utilizaba en escenas divertidas en los años 80. En su faceta de experto en artes marciales, fue quien le enseñó a Goku técnicas como el Kame Hame Ha",
+                     photo: URL(string: "https://cdn.alfabetajuega.com/alfabetajuega/2020/06/Roshi.jpg?width=300")!,
+                     favorite: false)
+            ],
             configureCellAction: { [weak self] cell, hero in
                 self?.configureCell(cell: cell, hero: hero)
             },
@@ -44,10 +50,15 @@ extension HeroesListViewController {
     }
     
     private func configureCell(cell: CharacterInfoTableViewCell, hero: Hero) {
-        cell.configure(imageUrl: hero.photo, name: hero.name, description: hero.description)
+        cell.configure(
+            imageUrl: hero.photo,
+            name: hero.name,
+            description: hero.description
+        )
     }
     
     private func didSelectItem(hero: Hero) {
-        print("Hero \(hero.name) did selected")
+        let heroDetailViewController = HeroDetailViewController(hero: hero)
+        navigationController?.show(heroDetailViewController, sender: nil)
     }
 }
