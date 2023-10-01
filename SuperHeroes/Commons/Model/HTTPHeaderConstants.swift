9e469b47ee6fe348d1f691959c8c59a9285f7668
DanyST <lherreralillo@gmail.com>
2023-10-01 17:28:55 -0300

    Add HTTPMethods and HTTPHeaderConstants enums

---
+++ SuperHeroes/Commons/Model/HTTPHeaderConstants.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPHeaderConstants.swift
+//  SuperHeroes
+//
+//  Created by Luis Herrera Lillo on 01-10-23.
+//
+
+import Foundation
+
+enum HTTPHeaderConstants: String {
+    case bearer = "Bearer"
+    case basic = "Basic"
+    case authorization = "Authorization"
+    case aplicationJSONContentType = "application/json"
+    case contentType =  "Content-Type"
+}
---
+++ SuperHeroes/Commons/Model/HTTPMethods.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPMethods.swift
+//  SuperHeroes
+//
+//  Created by Luis Herrera Lillo on 01-10-23.
+//
+
+import Foundation
+
+enum HTTPMethods: String {
+    case post = "POST"
+    case get = "GET"
+    case put = "PUT"
+}
