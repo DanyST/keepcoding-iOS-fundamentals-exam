e7a97c9c5cf4c3941b01bddc69d0f5e3e1f371ad
DanyST <lherreralillo@gmail.com>
2023-09-30 21:07:46 -0300

    add UITextField+SidePadding extension

---
+++ SuperHeroes/Extensions/UITextField+SidePadding.swift
@@ -0,0 +1,32 @@
+//
+//  UITextField+SidePadding.swift
+//  SuperHeroes
+//
+//  Created by Luis Herrera Lillo on 30-09-23.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat){
+        let paddingView = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: amount,
+            height: self.frame.size.height
+        ))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: amount,
+            height: self.frame.size.height
+        ))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
